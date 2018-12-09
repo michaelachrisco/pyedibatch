561b5d68ad5a58a2a30f4ae5629b056895d78c56
Michael Chrisco <michaelachrisco@gmail.com>
2018-12-08 20:03:37 -0800

    Init setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pyedibatch',
+    version='0.1dev',
+    packages=['pyedibatch',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
